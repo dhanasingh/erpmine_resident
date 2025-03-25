a5503cdfb66c0c53fc6f37fe819ce4e479d8a500
Mohan Raj <nk.mohan96@gmail.com>
2025-03-25 16:53:32 +0530

    updated init.rb

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :erpmine_resident do
   name 'ERPmine Resident Management plugin'
   author 'Adhi Software Pvt Ltd'
   description 'This is a plugin for Resident Management'
-  version '0.3.6'
+  version '1.0'
   url 'http://www.redmine.org/plugins/wk-time'
   author_url 'http://www.adhisoftware.co.in/'
 
