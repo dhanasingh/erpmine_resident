5da3d357fa26355ccc9aa7306c7d3b1d2e041312
Mohan Raj <nk.mohan96@gmail.com>
2026-07-29 12:32:41 +0530

    Sort issues by subject and id

--- app/helpers/rmperformservice_helper.rb
+++ app/helpers/rmperformservice_helper.rb
@@ -19,7 +19,8 @@ module RmperformserviceHelper
 include RmapartmentHelper
   def getResIssues
     issues = Array.new
-    allIssues = Issue.where(:project_id => controller.getDefultProject)
+    allIssues = Issue.where(:project_id => controller.getDefultProject).to_a
+      .sort_by { |i| [i.subject.to_s.downcase, i.id] }
     issues << [ "", ""] if !Setting.plugin_redmine_wktime['wktime_allow_blank_issue'].blank? &&
     Setting.plugin_redmine_wktime['wktime_allow_blank_issue'].to_i == 1
     allIssues.each do |i|
